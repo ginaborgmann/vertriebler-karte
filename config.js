04018906a9e3d76a80e58d9132ed0201b5aabf82
ginaborgmann <gina.borgmann@mysolarexpress.de>
2026-07-01 17:14:00 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Deine Supabase-Daten eintragen.
-// Project URL hast du bereits: https://gaoihgqhnoooljlfhxgw.supabase.co
-window.SUPABASE_URL = "https://gaoihgqhnoooljlfhxgw.supabase.co";
-window.SUPABASE_ANON_KEY = "HIER_DEINEN_PUBLISHABLE_KEY_EINFUEGEN";
