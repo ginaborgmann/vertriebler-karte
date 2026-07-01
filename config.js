b288005724acf9b9f85ba5d1aaaa4a60505b122a
ginaborgmann <gina.borgmann@mysolarexpress.de>
2026-07-01 17:32:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 1) SUPABASE_URL findest du in Supabase > Settings > General > Project URL
 // 2) SUPABASE_PUBLISHABLE_KEY findest du in Supabase > Settings > API Keys > Publishable key
 
-const SUPABASE_URL = "https://gaoihgqhnoooljlfhxgw.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_gh9Zka3gPp66MCelppzO9Q_GlFBGcRG";
+window.SUPABASE_URL = "https://gaoihgqhnoooljlfhxgw.supabase.co";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_gh9Zka3gPp66MCelppzO9Q_GlFBGcRG";
